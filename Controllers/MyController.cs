229a409668e8b4ae174e94ef20bc50b62dd316da
Macmini <macmini@Mac-mini-de-Macmini.local>
2022-06-20 15:41:05 -0300

    first commit

---
+++ Controllers/MyController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace PrimeiroBackend.Controllers
+{
+    [ApiController]
+    [Route("experimentos")]
+    public class MyController : ControllerBase
+    {
+    
+
+        private readonly ILogger<MyController> _logger;
+
+        public MyController(ILogger<MyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Meu primeiro metodo get";
+        }
+
+        [HttpPost]
+        public string Post([FromBody] JsonElement animal)
+        {
+            return animal.GetRawText();
+        }
+    }
+}
